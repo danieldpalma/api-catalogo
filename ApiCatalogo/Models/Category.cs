6bd4edd44485f05c477597ac182a02c8c3d4e0be
danieldpalma <danielpalma232@gmail.com>
2025-11-27 20:53:41 -0300

    feat: added data context, migrations and data annotations to models

--- ApiCatalogo/Models/Category.cs
+++ ApiCatalogo/Models/Category.cs
@@ -1,8 +1,18 @@
-﻿namespace ApiCatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ApiCatalogo.Models;
+
+[Table("Categories")]
 public class Category
 {
+	[Key]
 	public int CategoryId { get; set; }
+	[Required]
+	[StringLength(80)]
 	public string? Name { get; set; }
+	[Required]
+	[StringLength(300)]
 	public string? ImageUrl { get; set; }
+	public ICollection<Product>? Products { get; set; } = [];
 }
