653d50b862ee211d7a7c210872c419a57a739d0a
danieldpalma <danielpalma232@gmail.com>
2025-12-15 17:29:58 -0300

    feat: added dto to login, register, response and token models

---
+++ ApiCatalogo/DTOs/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.DTOs;
+
+public class LoginModel
+{
+	[Required(ErrorMessage = "Username is required.")]
+	public string? Name { get; set; }
+	[Required(ErrorMessage = "Password is required.")]
+	public string? Password { get; set; }
+}
---
+++ ApiCatalogo/DTOs/TokenModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiCatalogo.DTOs;
+
+public class TokenModel
+{
+	public string? AccessToken { get; set; }
+	public string? RefreshToken { get; set; }
+}
