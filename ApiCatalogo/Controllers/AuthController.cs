e22578a7096fa277787b9ddac48bbbca497a4075
danieldpalma <danielpalma232@gmail.com>
2026-01-13 14:10:56 -0300

    feat: added create policy endpoint

--- ApiCatalogo/Controllers/AuthController.cs
+++ ApiCatalogo/Controllers/AuthController.cs
@@ -17,13 +17,15 @@ public class AuthController : ControllerBase
 	private readonly UserManager<ApplicationUser> _userManager;
 	private readonly RoleManager<IdentityRole> _roleManager;
 	private readonly IConfiguration _configuration;
+	private readonly ILogger<AuthController> _logger;
 
-	public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+	public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, ILogger<AuthController> logger)
 	{
 		_tokenService = tokenService;
 		_userManager = userManager;
 		_roleManager = roleManager;
 		_configuration = configuration;
+		_logger = logger;
 	}
 
 	[HttpPost]
@@ -144,5 +146,29 @@ public class AuthController : ControllerBase
 		await _userManager.UpdateAsync(user);
 
 		return NoContent();
+	}
+
+	[HttpPost]
+	[Route("createRole")]
+	public async Task<IActionResult> CreateRole(string roleName)
+	{
+		var roleExist = await _roleManager.RoleExistsAsync(roleName);
+
+		if (!roleExist)
+		{
+			var roleResult = await _roleManager.CreateAsync(new IdentityRole(roleName));
+
+			if (roleResult.Succeeded)
+			{
+				_logger.LogInformation(1, "Role added");
+				return StatusCode(StatusCodes.Status201Created, new Response { Status = "Success", Message = $"Role {roleName} added successfully" });
+			}
+			else
+			{
+				_logger.LogInformation(2, $"Failed to create role: {roleName}");
+				return StatusCode(StatusCodes.Status400BadRequest, new Response { Status = "Error", Message = $"Issue adding the new {roleName} role" });
+			}
+		}
+		return StatusCode(StatusCodes.Status400BadRequest, new Response { Status = "Error", Message = $"Role already exist." });
 	}
 }
